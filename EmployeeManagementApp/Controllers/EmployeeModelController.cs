c8cafca7f6bab6639e27634d9703d128c7e1a940
Pratama <69pratama.aditya69@gmail.com>
2023-12-13 18:36:15 +0700

    Generate Controller
    Generate Controller for EmployeeModel and JobpositionModel, as well creating views for CRUD

---
+++ EmployeeManagementApp/Controllers/EmployeeModelController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagement.Models;
+using EmployeeManagementApp.Data;
+
+namespace EmployeeManagementApp.Controllers
+{
+    public class EmployeeModelController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeModelController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: EmployeeModel
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Employees.ToListAsync());
+        }
+
+        // GET: EmployeeModel/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employeeModel = await _context.Employees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employeeModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(employeeModel);
+        }
+
+        // GET: EmployeeModel/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeModel/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,MiddleName,LastName,DateOfBirth,Gender,Address")] EmployeeModel employeeModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employeeModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employeeModel);
+        }
+
+        // GET: EmployeeModel/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employeeModel = await _context.Employees.FindAsync(id);
+            if (employeeModel == null)
+            {
+                return NotFound();
+            }
+            return View(employeeModel);
+        }
+
+        // POST: EmployeeModel/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,MiddleName,LastName,DateOfBirth,Gender,Address")] EmployeeModel employeeModel)
+        {
+            if (id != employeeModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employeeModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeModelExists(employeeModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employeeModel);
+        }
+
+        // GET: EmployeeModel/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employeeModel = await _context.Employees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employeeModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(employeeModel);
+        }
+
+        // POST: EmployeeModel/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var employeeModel = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(employeeModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeModelExists(int id)
+        {
+            return _context.Employees.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ EmployeeManagementApp/Controllers/JobpositionModelController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagement.Models;
+using EmployeeManagementApp.Data;
+
+namespace EmployeeManagementApp.Controllers
+{
+    public class JobpositionModelController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public JobpositionModelController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: JobpositionModel
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.JobPositions.Include(j => j.Employee);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: JobpositionModel/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jobpositionModel = await _context.JobPositions
+                .Include(j => j.Employee)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (jobpositionModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(jobpositionModel);
+        }
+
+        // GET: JobpositionModel/Create
+        public IActionResult Create()
+        {
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Id");
+            return View();
+        }
+
+        // POST: JobpositionModel/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,JobName,StartDate,EndDate,Salary,IsActive,EmployeeId")] JobpositionModel jobpositionModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(jobpositionModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Id", jobpositionModel.EmployeeId);
+            return View(jobpositionModel);
+        }
+
+        // GET: JobpositionModel/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jobpositionModel = await _context.JobPositions.FindAsync(id);
+            if (jobpositionModel == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Id", jobpositionModel.EmployeeId);
+            return View(jobpositionModel);
+        }
+
+        // POST: JobpositionModel/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,JobName,StartDate,EndDate,Salary,IsActive,EmployeeId")] JobpositionModel jobpositionModel)
+        {
+            if (id != jobpositionModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(jobpositionModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!JobpositionModelExists(jobpositionModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Id", jobpositionModel.EmployeeId);
+            return View(jobpositionModel);
+        }
+
+        // GET: JobpositionModel/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jobpositionModel = await _context.JobPositions
+                .Include(j => j.Employee)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (jobpositionModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(jobpositionModel);
+        }
+
+        // POST: JobpositionModel/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var jobpositionModel = await _context.JobPositions.FindAsync(id);
+            _context.JobPositions.Remove(jobpositionModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool JobpositionModelExists(int id)
+        {
+            return _context.JobPositions.Any(e => e.Id == id);
+        }
+    }
+}
